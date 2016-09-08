b131b54e7bd41bc30cbbf7cdea09e513dc745e54
Arkolips <catel.ludovic@gmail.com>
2016-09-08 19:54:23 +0200

    add files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Catel Ludovic</title>
+    <link rel="stylesheet" href="private/vendor/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="private/vendor/font-awesome/css/font-awesome.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
+    <script src="private/vendor/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="public/js/script.js" charset="utf-8"></script>
+  </body>
+</html>
