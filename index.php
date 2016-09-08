dd98efd1a3f858051f913e7f643c4b513a16b1cc
Arkolips <catel.ludovic@gmail.com>
2016-09-08 23:29:59 +0200

    structure index.php

--- index.php
+++ index.php
@@ -8,6 +8,63 @@
     <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
   </head>
   <body>
+    <div class="container-fluid">
+
+      <div class="header">
+        <p>CATEL LUDOVIC</p>
+        <p>DEVELOPPEUR WEB JUNIOR</p>
+      </div><!-- /Header -->
+
+      <nav class="navbar navbar-default">
+        <!-- Responsive Nav -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div><!-- /Responsive Nav -->
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </nav><!-- /Nav -->
+
+      <main>
+        <div class="col-md-8 col-md-offset-2">
+          <h2>Bonjour et bienvenue sur mon CV ! </h2>
+          <p>
+            A l'origine formé dans le domaine de la Communication et des Industries Graphiques, c'est en 2015 que j'ai décidé de me réorienter dans le domaine du Web. <br>
+            Ne connaissant pas ce domaine, j'ai eu l'opportunité de participer à une initiation aux métiers du numériques au sein de la Cyber-Base d'Euratechnologies à Lille.<br>
+            A la suite de cette initiation, j'ai décidé de poursuivre ma formation en rejoignant la première session de Webforce3 à Lille. <br><br>
+            J'ai aujourd'hui pour affinité le développement frontend avec en particulier le langage JavaScript.
+          </p>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+          <div class="col-md-3">
+            <i class="fa fa-exchange fa-4x"></i>
+            <p>Expériences</p>
+          </div>
+          <div class="col-md-3">
+            <i class="fa fa-pied-piper fa-4x"></i>
+            <p>Compétences</p>
+          </div>
+          <div class="col-md-3">
+            <i class="fa fa-paint-brush fa-4x"></i>
+            <p>Création</p>
+          </div>
+          <div class="col-md-3">
+            <i class="fa fa fa-envelope fa-4x"></i>
+            <p>Contact</p>
+          </div>
+        </div>
+      </main>
+    </div><!-- /Container FLuid -->
 
     <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
     <script src="private/vendor/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
